b28e9762ddfc571e27ab8fad99d2b0df2f0ce50a
Zeusu Sato <zeusu.sato@dorodango.biz>
2025-08-06 05:01:38 +0000

    Updated spandas/enhanced/__init__.py

--- spandas/enhanced/__init__.py
+++ spandas/enhanced/__init__.py
@@ -1,1 +1,21 @@
 # spandas/enhanced/__init__.py
+
+from . import apply
+from . import aggregation
+from . import join
+from . import reshape
+from . import selection
+from . import missing
+from . import plot
+from . import mathstats  # ←ここはモジュールだからOK
+
+__all__ = [
+    "apply",
+    "aggregation",
+    "join",
+    "reshape",
+    "selection",
+    "missing",
+    "plot",
+    "mathstats"
+]
