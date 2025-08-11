22851f1800996c1e82abf6866fcb97100da6ef84
Zeusu Sato <zeusu.sato@dorodango.biz>
2025-08-11 23:45:16 +0900

    Add pyproject build metadata and dependencies

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(
     url='https://github.com/zeusu-sato/spandas',
     packages=find_packages(include=["spandas", "spandas.*"]),
     install_requires=[
-        'pyspark>=3.5.0,<4.0.0',
         'pandas>=1.5,<2.0',
         'numpy>=1.22,<2.0',
-        'swifter',
-        'matplotlib',
-        'pyarrow>=8.0.0,<13.0',
+        'pyarrow>=8,<13',
+        'matplotlib>=3.7,<3.8',
+        'swifter>=1.4,<1.5',
+        'dask[dataframe]>=2024.2,<2024.7',
+        'dask-expr>=1.0,<1.1',
     ],
     python_requires='>=3.7',
 )
